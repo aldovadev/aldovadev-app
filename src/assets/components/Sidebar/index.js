1d24e24ec31e5273e7d5be08e700ddb338a1204b
yunomi811 <aldova811@gmail.com>
2023-03-03 13:50:16 +0700

    membuat sidebar navigation

---
+++ src/assets/components/Sidebar/index.js
@@ -0,0 +1,36 @@
+import './index.scss'
+import { Link, NavLink } from 'react-router-dom'
+import LogoS from '../../images/logo-s.png'
+import LogoSubtitle from '../../images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () => (
+    <div className="nav-bar">
+        <Link className="logo" to="/">
+            <img src={LogoS} alt="logo" />
+            <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+        </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="4d4d4e" />
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/aldova-dev">
+                <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' /> 
+                </a>
+            </li>
+        </ul>
+    </div>
+)
+
+export default Sidebar
