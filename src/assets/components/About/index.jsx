86e3a4df90a6b5f78449e2521b23270ea9032f34
yunomi811 <aldova811@gmail.com>
2023-03-13 08:21:16 +0700

    menambahkan fitur map pada contact page

--- src/assets/components/About/index.jsx
+++ src/assets/components/About/index.jsx
@@ -35,22 +35,22 @@ const About = () => {
             <div className='stage-cube-container'>
                 <div className='cubespinner'>
                     <div className='face1'>
-                        <FontAwesomeIcon icon={faReact} color="#dd0031"/>
+                        <FontAwesomeIcon icon={faReact} color="#61DBFB"/>
                     </div>
                     <div className='face2'>
-                        <FontAwesomeIcon icon={faHtml5} color="#dd0031"/>
+                        <FontAwesomeIcon icon={faHtml5} color="#e34c26"/>
                     </div>
                     <div className='face3'>
-                        <FontAwesomeIcon icon={faCss3} color="#dd0031"/>
+                        <FontAwesomeIcon icon={faCss3} color="#264de4"/>
                     </div>
                     <div className='face4'>
-                        <FontAwesomeIcon icon={faSass} color="#dd0031"/>
+                        <FontAwesomeIcon icon={faSass} color="#cc6699"/>
                     </div>
                     <div className='face5'>
-                        <FontAwesomeIcon icon={faJsSquare} color="#dd0031"/>
+                        <FontAwesomeIcon icon={faJsSquare} color="#f0db4f"/>
                     </div>
                     <div className='face6'>
-                        <FontAwesomeIcon icon={faGitAlt} color="#dd0031"/>
+                        <FontAwesomeIcon icon={faGitAlt} color="tomato"/>
                     </div>
                 </div>
             </div>
--- src/assets/components/Contact/index.jsx
+++ src/assets/components/Contact/index.jsx
@@ -1,17 +1,40 @@
 import Loader from 'react-loaders'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
+    const refForm = useRef();
 
     useEffect(() => {
         const t = setTimeout(() => {
             setLetterClass('text-animate-hover');
-        }, 2500)
+        }, 2500);
         return () => { clearTimeout(t); }
-    }, [],)
+    }, [],);
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs.sendForm(
+            'service_ceujca4',
+            'template_xyznakg',
+            refForm.current,
+            'zRgU6VqWh6FyJdj3M'
+        )
+        .then(
+            (result) => {
+                alert('Message successfully sent!')
+                window.location.reload(false)
+            },
+            (error) => {
+                alert('Failed to send the message, please try again!')
+            })
+    }
+
 
     return(
         <>
@@ -30,19 +53,19 @@ const Contact = () => {
                         don't hesitate to contact me using below form either.
                     </p>
                     <div className='contact-form'>
-                        <form>
+                        <form ref={refForm} onSubmit={sendEmail}>
                             <ul>
                                 <li className='half'>
-                                    <input type='text' name='namme' placeholder='Name' required />
+                                    <input type='text' name='user_name' placeholder='Name' required />
                                 </li>
                                 <li className='half'>
-                                    <input type='email' name='email' placeholder='Email' required />
+                                    <input type='email' name='user_email' placeholder='Email' required />
                                 </li>
                                 <li>
-                                    <input type='text' name='subject' placeholder='Subject' />
+                                    <input type='text' name='subject' placeholder='Subject' required/>
                                 </li>
                                 <li>
-                                    <textarea type='' name='message' placeholder='Message'>
+                                    <textarea name='message' placeholder='Message' required>
                                     </textarea>
                                 </li>
                                 <li>
@@ -51,6 +74,23 @@ const Contact = () => {
                             </ul>
                         </form>
                     </div>
+                </div>
+                <div className='info-map'>
+                    Aldova Guswantri,
+                    <br/>
+                    Indonesia,
+                    <br/>
+                    address : Jn. Veteran, Koto Tangah, No.08 <br/>
+                    Padang <br/>
+                    <span>email : aldova811@gmail.com</span>
+                </div>
+                <div className='map-wrap'>
+                    <MapContainer center={[-0.867037, 100.337589]} zoom={13}>
+                        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                        <Marker position={[-0.867037, 100.337589]}>
+                            <Popup>Aldova live here, come over and say hi to me !</Popup>
+                        </Marker>
+                    </MapContainer>
                 </div>
             </div>
             <Loader type='pacman'/>
