091c6c8ded83e04fd9726eba67339d99a1b8e822
aldovadev <aldova811@gmail.com>
2024-03-14 03:04:21 +0700

    fix : about design and layout

--- src/assets/components/About/index.jsx
+++ src/assets/components/About/index.jsx
@@ -2,62 +2,282 @@ import './index.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
-import { faHtml5, faCss3, faGitAlt, faJsSquare, faReact, faAngular } from '@fortawesome/free-brands-svg-icons'
+import {
+  faHtml5,
+  faCss3,
+  faGitAlt,
+  faJsSquare,
+  faReact,
+  faAngular,
+} from '@fortawesome/free-brands-svg-icons'
 import Loader from 'react-loaders'
 
 const About = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
+  const [letterClass, setLetterClass] = useState('text-animate')
 
-    useEffect(() => {
-        const t = setTimeout(() => {
-            setLetterClass('text-animate-hover');
-        }, 3000)
-        return () => { clearTimeout(t); }
-    }, [],)
+  useEffect(() => {
+    const t = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+    return () => {
+      clearTimeout(t)
+    }
+  }, [])
 
-    return (
-        <>
-            <div className="container about-page">
-                <div className="text-zone" >
-                    <h1>
-                        <AnimatedLetters 
-                            letterClass={letterClass}
-                            strArray = {['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-                            idx = {15}
-                        />
-                    </h1>
-                    <p>Hi, I'm Aldova Guswantri, a Full Stack Software Engineer with a passion for designing and building innovative solutions that solve complex problems. I have experience working with a wide range of technologies and frameworks, and I thrive on the challenge of developing scalable, robust applications that meet the needs of clients and end-users alike.</p>
-                    <p>As a Full Stack Software Engineer, I am proficient in both front-end and back-end technologies. On the front-end, I specialize in react.js, qt, qml, sass, vb.net, using them to create dynamic user interfaces that provide a seamless experience across devices. On the back-end, I am skilled in node.js, postgreSQL, mySQL, firebase.</p>
-                    <p>I am committed to ongoing learning and development, and I stay up-to-date with the latest technologies and industry trends. I am always looking for opportunities to expand my skill set and explore new tools and frameworks.</p>
-                    <p>In my free time, I enjoy gaming and hiking, which help me recharge and bring fresh perspectives to my work. I am passionate about using my skills to make a positive impact on the world, and I believe in the power of technology to drive social change.</p>
-                </div>
-
-                <div className='stage-cube-container'>
-                    <div className='cubespinner'>
-                        <div className='face1'>
-                            <FontAwesomeIcon icon={faReact} color="#61DBFB"/>
-                        </div>
-                        <div className='face2'>
-                            <FontAwesomeIcon icon={faHtml5} color="#e34c26"/>
-                        </div>
-                        <div className='face3'>
-                            <FontAwesomeIcon icon={faCss3} color="#264de4"/>
-                        </div>
-                        <div className='face4'>
-                            <FontAwesomeIcon icon={faAngular} color="#dd1b16"/>
-                        </div>
-                        <div className='face5'>
-                            <FontAwesomeIcon icon={faJsSquare} color="#f0db4f"/>
-                        </div>
-                        <div className='face6'>
-                            <FontAwesomeIcon icon={faGitAlt} color="#F1502F"/>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="container about-page">
+        <div className="stage-cube-container">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faReact} color="#61DBFB" />
             </div>
-            <Loader type="pacman" />
-        </>
-    )
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#e34c26" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#264de4" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faAngular} color="#dd1b16" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#f0db4f" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#F1502F" />
+            </div>
+          </div>
+        </div>
+        <div className="text-container">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm Aldova Guswantri, a Professional Software Engineer skilled in
+            frontend, backend, DevOps, and DevSecOps. With a passion for
+            innovation, I excel at crafting elegant solutions to complex
+            problems using a diverse range of technologies. My collaborative
+            approach ensures the delivery of robust, scalable software solutions
+            that exceed expectations.
+          </p>
+
+          <h3> TECH STACK :</h3>
+          {/* Programming Languange */}
+          <div className="skill-container">
+            <img
+              src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white"
+              alt="hnorefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/kotlin-%237F52FF.svg?style=for-the-badge&logo=kotlin&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/c-%2300599C.svg?style=for-the-badge&logo=c&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/shell_script-%23121011.svg?style=for-the-badge&logo=gnu-bash&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/Cloudflare-F38020?style=for-the-badge&logo=Cloudflare&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            {/* Cloud Provider */}
+            <img
+              src="https://img.shields.io/badge/AWS-%23FF9900.svg?style=for-the-badge&logo=amazon-aws&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/GoogleCloud-%234285F4.svg?style=for-the-badge&logo=google-cloud&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/AWS-%23FF9900.svg?style=for-the-badge&logo=amazon-aws&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/DigitalOcean-%230167ff.svg?style=for-the-badge&logo=digitalOcean&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/azure-%230072C6.svg?style=for-the-badge&logo=microsoftazure&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/firebase-%23039BE5.svg?style=for-the-badge&logo=firebase"
+              alt="norefferer"
+              height="20px"
+            />
+            {/* Frameworks */}
+            <img
+              src="https://img.shields.io/badge/angular-%23DD0031.svg?style=for-the-badge&logo=angular&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/Qt-%23217346.svg?style=for-the-badge&logo=Qt&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/FastAPI-005571?style=for-the-badge&logo=fastapi"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/django-%23092E20.svg?style=for-the-badge&logo=django&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/opencv-%23white.svg?style=for-the-badge&logo=opencv&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            {/* Database */}
+            <img
+              src="https://img.shields.io/badge/mysql-%2300000f.svg?style=for-the-badge&logo=mysql&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/Firebase-039BE5?style=for-the-badge&logo=Firebase&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/redis-%23DD0031.svg?style=for-the-badge&logo=redis&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/Supabase-3ECF8E?style=for-the-badge&logo=supabase&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/nginx-%23009639.svg?style=for-the-badge&logo=nginx&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/kubernetes-%23326ce5.svg?style=for-the-badge&logo=kubernetes&logoColor=white"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/Apache%20Kafka-000?style=for-the-badge&logo=apachekafka"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/-ElasticSearch-005571?style=for-the-badge&logo=elasticsearch"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/sonarqube-4E9BCD.svg?style=for-the-badge&logo=sonarqube&logoColor=white&color=%234E9BCD"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/vault-FFEC6E.svg?style=for-the-badge&logo=vault&logoColor=white&color=%23FFEC6E"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/grafana-F46800.svg?style=for-the-badge&logo=grafana&logoColor=white&color=%23F46800"
+              alt="norefferer"
+              height="20px"
+            />
+            <img
+              src="https://img.shields.io/badge/argo-EF7B4D.svg?style=for-the-badge&logo=argo&logoColor=white&color=%23EF7B4D"
+              alt="norefferer"
+              height="20px"
+            />
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
 }
 
 export default About
