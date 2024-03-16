3db9f90e3c51ead50c8629700084de875b691df0
aldovadev <aldova811@gmail.com>
2024-03-17 03:38:14 +0700

    add : temp cf.sh

---
+++ cf.sh
@@ -0,0 +1,5 @@
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+
+sudo dpkg -i cloudflared.deb && 
+
+sudo cloudflared service install eyJhIjoiZDczNGQ2NDgyYTZhMzc5NGNmZTkyOTgzYjk2MTg3NjAiLCJ0IjoiZTk1MGIyNDEtZTA1YS00OTY5LWE4MWEtMjA3NWFmODRlYWNlIiwicyI6IllqazJNemswWWpndE1XWXlOeTAwTjJVNExUbGtNR1l0WmpKaVlXTmxNbU13WXpBMSJ9
